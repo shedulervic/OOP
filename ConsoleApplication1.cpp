9a9ff5028e4fb47e189d0505980740d91c53ef2a
shedulervic <sheduler.vic@gmail.com>
2021-09-09 16:42:57 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,249 +1,239 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <cassert>
-#include <algorithm>
-#include <array>
 using namespace std;
 
-
-int comp1(const void *a, const void *b)
+class Card
 {
-	return (*(int*)a - *(int*)b);
+public:
+    enum rank {
+        ACE = 1, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN,
+        JACK, QUEEN, KING
+    };
+    enum suit { CLUBS, DIAMONDS, HEARTS, SPADES };
+
+    Card(rank r = ACE, suit s = SPADES, bool ifu = true);
+    int GetValue() const;
+    void Flip();
+
+private:
+    rank m_Rank;
+    suit m_Suit;
+    bool m_IsFaceUp;
+};
+
+Card::Card(rank r, suit s, bool ifu) : m_Rank(r), m_Suit(s), m_IsFaceUp(ifu)
+{}
+
+int Card::GetValue() const
+{
+    int value = 0;
+    if (m_IsFaceUp)
+    {
+        value = m_Rank;
+        if (value > 10)
+        {
+            value = 10;
+        }
+    }
+    return value;
 }
 
-class Card // емли я правильно понял то здесь только сама карта, колода отдельным объектом?
+void Card::Flip()
 {
-private:
-	
-public:
-	bool m_position;
-	enum Suit
-	{
-		spades, clubs, hearts, diamonds
-	};
-	enum cardValue
-	{
-		TWO, THREE, FOUR, FIVE, SIX, SEVEN,
-		EIGHT, NINE, TEN, ACE, KING, QUEEN, JACK
-	};
-	Suit m_card_suit;
-	cardValue m_card_value;
-	Card(Suit suit, cardValue value, bool position) {
-		m_card_suit = suit;
-		m_card_value = value;
-		m_position = position;
-	};
-	void Flip() {
-		m_position = !m_position;
-	};
-	cardValue GetValue() const {
-		return m_card_value;
-	};
-	~Card() {};
-};
+    m_IsFaceUp = !(m_IsFaceUp);
+}
 
 class Hand
 {
-private:
-	vector<Card*> hand;
-	int pointArr[13]{ 2,3,4,5,6,7,8,9,10,1,10,10,10 };
 public:
-	Hand(Card *card) {
-		hand.push_back(card);
-	};
-	void handAdd(Card *card) {
-		hand.push_back(card);
-	};
-	void handPrint() {
-		if (hand.empty())
-		{
-			cout << "рука пуста" << endl;
-		}
-		else
-		{
-			string suit[]{ "spades", "clubs", "hearts", "diamonds" };
-			string cardvalue[]{"TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN", "ACE", "KING", "QUEEN", "JACK"
-			};
-			for (auto& v : hand)
-			{
-				cout << suit[v->m_card_suit] << "|" << cardvalue[v->GetValue()] << "|" << v->m_position << endl;
-			}
+    Hand();
+    // виртуальный деструктор
+    virtual ~Hand();
 
-		}
-			};
-	void GetValue() {
-		int value = 0;
-		int a = 0;
-		for (auto& v : hand)
-		{
-			value += pointArr[v->GetValue()];
-			if (pointArr[v->GetValue()] == 1)
-			{
-				a = a + 1;
-			}
-		}
-		if (a > 0)
-		{
-			for (a > 0; a--;)
-			{
-				if (value < 11)
-				{
-					value = value + 10;
-				}
-			}
-		}
-		//for (int i = 0; i < hand.size(); i++)
-		//{
-		//	if (hand[i]->GetValue() == 1)
-		//	{
-		//		a + 1;
-		//	}
-		//	else
-		//	{
-		//	/*	value += pointArr[];*/
-		//	}
-		//}
-		//if (a > 0)
-		//{
-		//	for (a > 0; a--;)
-		//	{
-		//		if (value < 11)
-		//		{
-		//			value + 10;
-		//		}
-		//	}
-		//}
-		
-		cout << "очков: " << value << endl;
-	};
-	~Hand() {
-		hand.clear();
-	};
+    // добавляет карту в руку
+    void Add(Card* pCard);
+
+    // очищает руку от карт
+    void Clear();
+
+    //получает сумму очков карт в руке, присваивая тузу
+    // значение 1 или 11 в зависимости от ситуации
+    int GetTotal() const;
+
+protected:
+    vector<Card*> m_Cards;
 };
 
-class ArrayInt
+Hand::Hand()
 {
-private:
-	int *m_data;
-	int m_length;
-public:
-	ArrayInt() : m_length(0), m_data(nullptr) {
+    m_Cards.reserve(7);
+}
+// деструктор по-прежнему виртуальный
+// это уже можно не обозначать
+Hand::~Hand()
+{
+    Clear();
+}
 
-	}
-	ArrayInt(int lenght) : m_length(lenght) {
-		assert(lenght >= 0);
-		if (lenght > 0)
-		{
-			m_data = new int[lenght];
-		}
-		else
-		{
-			m_data = nullptr;
-		}
-	}
-	~ArrayInt() {
-		delete[] m_data;
-	}
-	void clear() {
-		delete[] m_data;
-		m_data = nullptr;
-		m_length = 0;
-	}
-	int size() {
-		return m_length;
-	}
-	int& operator[](int index) {
-		assert(index >= 0 && index < m_length);
-		return m_data[index];
-	}
-	void resize(int newLength) {
-		if (newLength == m_length)
-			return;
-		if (newLength <= 0)
-			clear();
-		return;
-		int *data = new int[newLength];
-		int elementsToCopy = newLength > m_length ? m_length : newLength;
-		for (int i = 0; i < elementsToCopy; ++i)
-		{
-			data[i] = m_data[i];
-		}
-		delete[] m_data;
-		m_data = data;
-		m_length = newLength;
-	}
-	void insertBefore(int value, int index) {
-		//Проверка корректности индекса
-		assert(index >= 0 && index <= m_length);
-		//создаем массив +1 к старому
-		int *data = new int(m_length + 1);
-		// Копируем элементы до индекса
-		for (int before = 0; before < index; ++before)
-			data[before] = m_data[before];
-		// вставляем элемент
-		data[index] = value;
-		//копируем элементы после всталяемого
-		for (int after = index; after < m_length; ++after)
-			data[after + 1] = m_data[after];
-		//Удаляем старый массив и оставляем новый
-		delete[] m_data;
-		m_data = data;
-		++m_length;
-	}
-	//задание 1
-	int popBack() {
-		return m_data[m_length];
-		m_length--;
-	}
-	int popFront() {
-		return m_data[0];
-		int* data = new int(m_length - 1);
-		for (int index = 0; index < m_length - 1; index++)
-			data[index] = m_data[index + 1];
-	}
-	void sortArr() {
-		qsort(m_data, m_length, sizeof(int), comp1);
-	}
-	void print(int index) {
-		(assert(index >= 0 && index <= m_length));
-		cout << m_data[index] << endl;
-	}
-	void push_back(int value) {
-		insertBefore(value, m_length);
-	}
-	void getValue() {
-	
-	};
+void Hand::Add(Card* pCard)
+{
+    m_Cards.push_back(pCard);
+}
+
+int Hand::GetTotal() const
+{
+    // если карт в руке нет, возвращает значение 0
+    if (m_Cards.empty())
+    {
+        return 0;
+    }
+
+    //если первая карта имеет значение 0, то она лежит рубашкой вверх:
+    // вернуть значение 0
+    if (m_Cards[0]->GetValue() == 0)
+    {
+        return 0;
+    }
+
+    // находит сумму очков всех карт, каждый туз дает 1 очко
+    int total = 0;
+    vector<Card*>::const_iterator iter;
+    for (iter = m_Cards.begin(); iter != m_Cards.end(); ++iter)
+    {
+        total += (*iter)->GetValue();
+    }
+
+    // определяет, держит ли рука туз
+    bool containsAce = false;
+    for (iter = m_Cards.begin(); iter != m_Cards.end(); ++iter)
+    {
+        if ((*iter)->GetValue() == Card::ACE)
+        {
+            containsAce = true;
+        }
+    }
+
+    // если рука держит туз и сумма довольно маленькая, туз дает 11 очков
+    if (containsAce && total <= 11)
+    {
+        // добавляем только 10 очков, поскольку мы уже добавили
+        // за каждый туз по одному очку
+        total += 10;
+    }
+
+    return total;
+}
+
+// Задание 1
+template <class T>
+class Pair1 {
+private:
+    T m_a;
+    T m_b;
+public:
+    Pair1(const T& a, const T& b){
+        m_a = a;
+        m_b = b;
+    }
+    ~Pair1() {
+        
+    }
+    T& first() {
+        return m_a;
+    }
+    const T& first() const{
+    return m_a;
+    }
+    T& second(){
+        return m_b;
+    }
+    const T& second() const {
+        return m_b;
+    }
+};
+
+//Задание 2
+template<class T, class S>
+class Pair {
+private:
+    T m_t;
+    S m_s;
+public:
+    Pair(T t, S s) {
+        m_t = t;
+        m_s = s;
+    }
+    T first() {
+        return m_t;
+    }
+    S second() {
+        return m_s;
+    }
+};
+
+//Задание 3
+template <class V>
+class StringValuePair : public Pair<std::string, V>
+{
+public:
+    StringValuePair(const std::string& str, const V& data)
+        : Pair<std::string, V>(str, data){
+    }
+};
+
+//Задание 4
+class GenericPlayer {
+private:
+	string g_name;
+    bool busted;
+    int total;
+public:
+    GenericPlayer(string name) {
+        g_name = name;
+        busted = false;
+    }
+    virtual bool IsHitting() {
+        if (total < 21){
+            return true;
+        }
+        else{
+            false;
+        }
+    }
+    bool IsBoosted() {
+        total = Hand().GetTotal();
+        if (total > 21){
+            busted = true;
+            return busted;
+        }
+    }
+    void Bust() {
+        cout << "Игрок: " << g_name << "ПЕРЕБОР!" << endl;
+    }
 };
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
-	/*ArrayInt arr(3);
-	int a = 1;
-	arr.insertBefore(a, 0);
-	arr.print(0)*/; // может вы подскажете что здесь не так? гдето поппутал с ссылками?
+    //Задание 1
+    Pair1<int> p1(6, 9);
+    cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
 
-	//Задание 2 
-	vector<int> v{ 1,5,5,5,4 };
-	sort(v.begin(), v.end());
-	int results = unique(v.begin(), v.end()) - v.begin();
-	cout << results << endl;
-	//Задание 3
-	Card card(Card::Suit::clubs, Card::cardValue::THREE, 0);
-	Card card2(Card::Suit::hearts, Card::cardValue::ACE, 0);
-	Card card3(Card::Suit::diamonds, Card::cardValue::JACK, 0);
-	Card card4(Card::Suit::hearts, Card::cardValue::FIVE, 0);
-	Hand hand(&card);
-	hand.handAdd(&card2);
-	hand.handAdd(&card3);
-	hand.handAdd(&card4);
-	hand.handPrint();
-	hand.GetValue();
-	hand.~Hand();
-	hand.handPrint();
+    const Pair1<double> p2(3.4, 7.8);
+    cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+    //Задание 2
+    Pair<int, double> p3(6, 7.8);
+    cout << "Pair2: " << p3.first() << ' ' << p3.second() << '\n';
+
+    Pair<double, int> p4(3.4, 5);
+    cout << "Pair2: " << p4.first() << ' ' << p4.second() << '\n';
+
+    //Задание 3
+    StringValuePair<int> svp("Amazing", 7);
+    std::cout << "Pair: " << svp.first() << ' ' << svp.second() << '\n';
 }
+
+
 
 
 
